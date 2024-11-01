348edb9a6873e80b73d168ef965d7b4d1a15ee8f
Kummirai <ajaxmilton@hotmail.com>
2024-11-01 18:40:14 +0200

    Refactor App component and add Header component with navigation

---
+++ my portifolio/src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <span>Milton </span>Kumirai
+      </h1>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About Me</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>{" "}
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
