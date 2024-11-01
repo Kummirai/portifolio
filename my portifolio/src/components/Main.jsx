dc3581f649cc152b37c727a7c811a877289e2c5c
Kummirai <ajaxmilton@hotmail.com>
2024-11-01 19:11:24 +0200

    Add Main component and update App layout with new styles

---
+++ my portifolio/src/components/Main.jsx
@@ -0,0 +1,25 @@
+const Main = () => {
+  return (
+    <main>
+      <section className="main">
+        <p>Hello Mate</p>
+        <h2>I&apos;m Milton Kumirai</h2>
+        <p>
+          a <span>Web Developer</span>
+        </p>
+        <p className="description">
+          Creative and dedicated Web Developer skilled in HTML, CSS, JavaScript,
+          and React. Passionate about designing responsive, user-friendly web
+          applications. I thrive on transforming complex problems into dynamic,
+          intuitive solutions. Always eager to learn and adopt the latest web
+          technologies. Let&apos;s build the future of the web together
+        </p>
+      </section>
+      <section className="page-image">
+        <img src="249.png" alt="" />
+      </section>
+    </main>
+  );
+};
+
+export default Main;
